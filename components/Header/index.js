ee098e839b0023897e668fcb3e5f4952791ec3f4
Brendan O'Brien <obrienpbrendan@gmail.com>
2018-03-24 06:28:53 -0600

    Header shared

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,52 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import Loading from '../Loading';
 
+import goBack from '../../navigation/goBack';
+import goHome from '../../navigation/goHome';
+
 // TODO: screenText/background in Redux State
 const backgroundColor = '#9F64C0';
-const Header = ({
-  children,
-  history,
-  loading,
-  location,
-  screenText = 'Menu',
-  color = 'red',
-}) => (
-  <View style={{ flex: 1 }}>
-    <View>
-      {location.pathname !== '/' ? (
-        <View style={headerStyle}>
-          <TouchableOpacity onPress={() => history.goBack()}>
-            <Text style={headerTextStyle}>{'<'}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => history.push('/')}>
-            <Text style={headerTextStyle}>Gustar</Text>
-          </TouchableOpacity>
-          <View>
-            <Text style={[headerTextStyle, { color: backgroundColor }]}>
-              {'<'}
-            </Text>
+const Header = props => {
+  const { children, loading } = props;
+  const color = 'red';
+  const screenText = 'Menu';
+  let rootBool, backFunc, homeFunc;
+  if (Platform.OS === 'web') {
+    const { history, location } = props;
+    rootBool = location.pathname !== '/';
+    backFunc = () => history.goBack();
+    homeFunc = () => history.push('/');
+  } else {
+    const { goBack, goHome, nav } = props;
+    rootBool = nav.index !== 0;
+    backFunc = goBack;
+    homeFunc = goHome;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View>
+        {rootBool ? (
+          <View style={headerStyle}>
+            <TouchableOpacity onPress={backFunc}>
+              <Text style={headerTextStyle}>{'<'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={homeFunc}>
+              <Text style={headerTextStyle}>Gustar</Text>
+            </TouchableOpacity>
+            <View>
+              <Text style={[headerTextStyle, { color: backgroundColor }]}>
+                {'<'}
+              </Text>
+            </View>
           </View>
-        </View>
-      ) : (
-        <View style={headerStyle}>
-          <View />
-          <Text style={headerTextStyle}>Gustar</Text>
-          <View />
-        </View>
-      )}
-      {loading ? null : (
-        <View style={subHeaderStyle}>
-          <Text style={[subHeaderTextStyle, { color }]}>{screenText}</Text>
-        </View>
-      )}
+        ) : (
+          <View style={headerStyle}>
+            <View />
+            <Text style={headerTextStyle}>Gustar</Text>
+            <View />
+          </View>
+        )}
+        {loading ? null : (
+          <View style={subHeaderStyle}>
+            <Text style={[subHeaderTextStyle, { color }]}>{screenText}</Text>
+          </View>
+        )}
+      </View>
+      {loading ? <Loading /> : children}
     </View>
-    {loading ? <Loading /> : children}
-  </View>
-);
+  );
+};
 
 const {
   headerStyle,
@@ -79,6 +99,10 @@ const {
   },
 });
 
-const mapStateToProps = ({ loading }) => ({ loading });
+const mapStateToProps = ({ loading, nav }) => ({ loading, nav });
+const HeaderComponent = Platform.OS === 'web' ? withRouter(Header) : Header;
 
-export default connect(mapStateToProps)(withRouter(Header));
+export default connect(mapStateToProps, {
+  goBack,
+  goHome,
+})(HeaderComponent);
